f983520696104efd38338c83f17ac42a71fbab78
Anton Kulikov <akulikov@jpl.nasa.gov>
2018-05-07 16:44:19 -0700

    Geometries library fully XML-ized.

--- src/library_geometries/parse-library-geometries.js
+++ src/library_geometries/parse-library-geometries.js
@@ -3,8 +3,8 @@ module.exports = ParseLibraryGeometries
 function ParseLibraryGeometries (library_geometries, xmlDae) {
   
   var outGeometries = []
-  var internalGeometries = []
-  var inGeometries = library_geometries[0].geometry
+//  var internalGeometries = []
+//  var inGeometries = library_geometries[0].geometry
 
   var geomNames = []
   var daeIterator = colladaXML.evaluate('/COLLADA/library_geometries/geometry', colladaXML, null, XPathResult.ANY_TYPE, null );
@@ -14,41 +14,34 @@ function ParseLibraryGeometries (library_geometries, xmlDae) {
     geomElement = daeIterator.iterateNext();  
   }
   console.log(geomNames);
-
-
   
+  // TODO: Bring this back when more comfortable with XML
   // split multiple connectivity lists into multiple geometries
-  inGeometries.forEach(function (library_geometry) {
-    var geometryMesh = library_geometry.mesh[0]
-    var indexList = geometryMesh.polylist || geometryMesh.triangles || null
-    if (!indexList) {
-      console.error("Geometry must contain either 'polylist' or 'triangles' object.")
-      return false
-    }
+//  inGeometries.forEach(function (library_geometry) {
+//    var geometryMesh = library_geometry.mesh[0]
+//    var indexList = geometryMesh.polylist || geometryMesh.triangles || null
+//    if (!indexList) {
+//      console.error("Geometry must contain either 'polylist' or 'triangles' object.")
+//      return false
+//    }
+//
+//    indexList.forEach(function (subList) {
+//      var newGeometry = JSON.parse(JSON.stringify(library_geometry));
+//      var internalIndexList = newGeometry.mesh[0].polylist || newGeometry.mesh[0].triangles
+//
+//      //TODO: Make compatible with polylist
+//      newGeometry.mesh[0].triangles = [subList]
+//
+//      internalGeometries.push(newGeometry)
+//    })
+//  })
 
-    indexList.forEach(function (subList) {
-      var newGeometry = JSON.parse(JSON.stringify(library_geometry));
-      var internalIndexList = newGeometry.mesh[0].polylist || newGeometry.mesh[0].triangles
-
-      //TODO: Make compatible with polylist
-      newGeometry.mesh[0].triangles = [subList]
-
-      internalGeometries.push(newGeometry)
-    })
-  })
-
-  internalGeometries.forEach(function (library_geometry, i) {
-    var geometryMesh = library_geometry.mesh[0]
-    var source = geometryMesh.source
-
-    var indexList = geometryMesh.polylist || geometryMesh.triangles || null
-
+  geomNames.forEach(function (geomName) {
     // Get index list offsets for vertex data - vertex, normal, texcoord
     var offsets = {}
     var maxOffset = 0
     
-    console.log(geomNames);
-    var daeIterator = colladaXML.evaluate('/COLLADA/library_geometries/*[@id="'+geomNames[i]+'"]/mesh/triangles/input', colladaXML, null, XPathResult.ANY_TYPE, null );
+    var daeIterator = colladaXML.evaluate('/COLLADA/library_geometries/*[@id="'+geomName+'"]/mesh/triangles/input', colladaXML, null, XPathResult.ANY_TYPE, null );
     var daeInput = daeIterator.iterateNext();
     while (daeInput) {
       var offset = parseInt(daeInput.getAttribute('offset'))
@@ -57,14 +50,8 @@ function ParseLibraryGeometries (library_geometries, xmlDae) {
       daeInput = daeIterator.iterateNext();
     }
 
-//    indexList[0].input.forEach(function (input) {
-//      var offset = parseInt(input.$.offset)
-//      offsets[input.$.semantic.toLowerCase()] = offset
-//      maxOffset = Math.max(maxOffset, offset)
-//    })
-
     /* Vertex Positions, UVs, Normals */
-    var polylistIndices = indexList[0].p[0].trim().split(' ')
+    var polylistIndices = colladaXML.evaluate('/COLLADA/library_geometries/*[@id="'+geomName+'"]/mesh/triangles/p', colladaXML, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue.innerHTML.trim().split(' ');
 
     var vertexNormalIndices = []
     var vertexPositionIndices = []
@@ -80,7 +67,16 @@ function ParseLibraryGeometries (library_geometries, xmlDae) {
     }
 
     vertexPropertyBuffers = {}
-    source.forEach( function (d) { vertexPropertyBuffers[d.$.id.split('-').pop()] = d.float_array[0]._.split(' ').map(Number); });
+    var daeIterator = colladaXML.evaluate('/COLLADA/library_geometries/*[@id="'+geomName+'"]/mesh/source', colladaXML, null, XPathResult.ANY, null ); 
+    var daeElement = daeIterator.iterateNext();
+    console.log(daeElement);
+    while (daeElement) {
+      var id = daeElement.getAttribute('id').split('-').pop();
+      var value = daeElement.firstElementChild.innerHTML.trim().split(' ').map(Number);
+      vertexPropertyBuffers[id] = value;
+      daeElement = daeIterator.iterateNext();
+    }
+
 
     var vertexPositions = vertexPropertyBuffers["positions"];
     var vertexNormals = vertexPropertyBuffers["normals"];
@@ -93,8 +89,8 @@ function ParseLibraryGeometries (library_geometries, xmlDae) {
     }
 
     outGeometries.push ({
-      id: library_geometry.$.id,
-      material: library_geometry.mesh[0].triangles[0].$.material,
+      id: geomName,
+      material: colladaXML.evaluate('/COLLADA/library_geometries/*[@id="'+geomName+'"]/mesh/triangles', colladaXML, null, XPathResult.FIRST_ORDERED_NODE_TYPE, null ).singleNodeValue.getAttribute('material'),
       vertexPositions: vertexPositions,
       vertexNormals: vertexNormals,
       vertexTangents: vertexTangents,
